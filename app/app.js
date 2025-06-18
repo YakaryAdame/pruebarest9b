f9e348268ce51dd7ec9b97065dd4be523995fc71
YakaryAdame <2236000505@utna.edu.mx>
2025-06-18 17:51:46 -0600

    se realizo el primer router de la api

--- app/app.js
+++ app/app.js
@@ -1,4 +1,14 @@
-const express = require('express');
+
+const express = require('express')
 const app = express();
+const router = require('./routes/gymRouter')
+
+
+//Desencriptar la informacion y manejar en formato json
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
+//Ejecutar router
+app.use('/gym',router)
 
 module.exports=app;
